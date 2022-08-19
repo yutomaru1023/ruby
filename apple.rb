c536b33a845e77318ca1a008c46c400aa62e6692
EC2 Default User <ec2-user@ip-172-31-33-190.ap-northeast-1.compute.internal>
2022-08-19 08:34:07 +0000

    ruby chapter6

--- apple.rb
+++ apple.rb
@@ -6,4 +6,4 @@ elsif apple=="Nagano"
   puts"このリンゴは青森県産ではなく、長野県産です。"
 else
   puts"このリンゴは青森県産でも長野県産でもありません。"
-end 
+end
---
+++ lesson5.rb
@@ -0,0 +1,14 @@
+total_price=101
+
+if total_price==100
+  puts "みかんを購入。所持金は0円。"
+end
+
+if total_price>100
+  puts"みかんを購入。所持金に余りあり。"
+end
+
+if total_price<100
+  puts"みかんを購入することができません。"
+end
+
